29c15b89a8e7c015920bfe9e561914b45fa87e9b
Aldeon Moriak <aldeonmoriak@gmail.com>
2019-04-27 01:42:32 +0430

    added person, made some changes on app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
@@ -7,6 +8,9 @@ function App() {
       <header className="App-header">
         <h1>Hi, I am a React App</h1>
       </header>
+      <Person name="Aldeon" age="27"/>
+      <Person name="Adonel" age="27" >Shit</Person>
+      <Person name="Saeed" age="29">Hobbies: Fucking, Playing Soccer and Playing Setar</Person>
     </div>
   );
 }
